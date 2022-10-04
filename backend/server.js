651524a123059cdf41acddf4f9958f90ea6d6f46
Yash Gupta <yashgupta13.in@gmail.com>
2022-10-04 11:11:54 +0530

    Commit to Main

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+app.get('/', (req,res) => {
+    res.send("API is running")
+})
+
+const PORT = process.env.PORT || 7000
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
