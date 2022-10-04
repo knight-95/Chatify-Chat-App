402b06b34e7beb26018b0361adad2b0931112a64
Yash Gupta <yashgupta13.in@gmail.com>
2022-10-04 17:45:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const app = express();
 
-dotenv.config();
+// dotenv.config();
 
 app.get('/', (req,res) => {
     res.send("API is running")
 })
 
-const PORT = process.env.PORT || 7000
-app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
+// const PORT = process.env.PORT || 7000
+app.listen(3000,console.log("Server started on PORT 3000"));
