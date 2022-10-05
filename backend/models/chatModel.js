2cf6d689638b89fcbb7a3e16d227e64f17fb42bf
Yash Gupta <yashgupta13.in@gmail.com>
2022-10-05 14:52:03 +0530

    Included Mongoose Models

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -5,3 +5,30 @@ list of users
 latestMessage
 groupAdmin */
 
+const mongoose = require('mongoose')
+const chatModel = mongoose.Schema(
+    {
+        // trim implies there are no trailing spaces behind and after
+        chatName: { type: String, trim: true},
+        isGroupChat: {type: Boolean, default:false},
+
+        // it has to be an array since a single chat will have two users and Group chat will have more than two users
+        users:[{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+        },
+        groupAdmin:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+const Chat = mongoose.model("Chat", chatModel);
+module.exports = Chat;
