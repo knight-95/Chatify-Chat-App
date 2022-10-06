7b848a2961ff3abf5819f0e80004b666169a8546
Yash Gupta <yashgupta13.in@gmail.com>
2022-10-06 14:27:33 +0530

    Connected with MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,11 @@ const connectDB = async() => {
         const conn = await mongoose.connect(MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: true
         });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     }catch(error){
-
+        console.log(`Error: ${error.message}`);
+        process.exit();
     }
 }
+module.exports = connectDB;
