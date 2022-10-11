7f666558646b84f2dcd343fe9dd297b818d8135f
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-10-11 13:08:06 +0530

    added package.json changes added Script changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
-const MONGO_URI="mongodb+srv://chaitfy:mongo123@cluster0.fz6bmoq.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require('mongoose')
 
-const connectDB = async() => {
-    try{
-        const conn = await mongoose.connect(MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    }catch(error){
-        console.log(`Error: ${error.message}`);
-        process.exit();
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log(`MongoDB connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(`Error: ${error.message}`)
+    process.exit()
+  }
 }
-module.exports = connectDB;
+module.exports = connectDB
