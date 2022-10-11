a2bf0ff7d994087c43d34f87166f23c15e6d03c3
Yash Gupta <79582395+knight-95@users.noreply.github.com>
2022-10-11 21:55:50 +0530

    Revert "added package.json changes added Script changes"

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const { chats } = require('./data/data')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
-const app = express()
+const express = require('express');
+const { chats } = require('./data/data');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const app = express();
 const userRoutes = require('./routes/userRoutes')
-dotenv.config({ path: './backend/.env' })
-connectDB()
+dotenv.config();
+connectDB();
 
-app.use(express.json()) //to accept json data
+app.use(express.json()); //to accept json data
 
-app.get('/', (req, res) => {
-  res.send('API is running')
+app.get('/', (req,res) => {
+    res.send("API is running")
 })
 
 // app.get('/api/chat/:id', (req,res) =>{
 //     // console.log(req.params.id);
 
-//     // comparing id's of each of the chats
+//     // comparing id's of each of the chats 
 //     const singleChat = chats.find(c=> c._id===req.params.id);
 //     res.send(singleChat);
 // })
-app.use('/api/user', userRoutes)
+app.use('/api/user',userRoutes)
 
-const port = process.env.PORT || 5000
-app.listen(port, console.log('`Server started on PORT 5000`'))
+const PORT = process.env.PORT;
+app.listen(5000,console.log("`Server started on PORT 5000`"));
